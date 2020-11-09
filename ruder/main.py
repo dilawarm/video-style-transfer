35eb1fc2679d5d95f2b93d1ed00338b33f3923a8
Dilawar Mahmood <dilawarmm@outlook.com>
2020-11-09 22:51:31 +0100

    deepflow and yuv

--- ruder/main.py
+++ ruder/main.py
@@ -1,30 +1,223 @@
-import time
 import os
-
-import IPython.display as display
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import PIL
+import time
+import functools
+import IPython.display as display
 import cv2
-
-from model import StyleContentModel
-from preprocessing import (
-    load_img,
-    imshow,
-    clip_0_1,
-    tensor_to_image,
-    load_video,
-    convert_to_video,
-    get_flow,
-)
-from utils import total_variation_loss
-
-frame_interval = 0.1
-TOL = 0.05
-loss_tolerance = 10000
+import tensorflow_addons as tfa
+from tqdm import tqdm
+from subprocess import check_output
 
 mpl.rcParams["figure.figsize"] = (12, 12)
 mpl.rcParams["axes.grid"] = False
+os.environ["TFHUB_MODEL_LOAD_FORMAT"] = "COMPRESSED"
+
+frame_interval = 0.1
+loss_tolerance = 1000
+
+TOL = 0.003
+
+keep_colors = False
+
+
+def tensor_to_image(tensor):
+    tensor = tensor * 255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor) > 3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+        return PIL.Image.fromarray(tensor)
+
+
+def load_img(path_to_img):
+    max_dim = 512
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+
+    yuv = cv2.cvtColor(np.float32(img[0].numpy()), cv2.COLOR_RGB2YUV)
+
+    return img, yuv
+
+
+def imshow(image, title=None):
+    if len(image.shape) > 3:
+        image = tf.squeeze(image, axis=0)
+
+    plt.imshow(image)
+    if title:
+        plt.title(title)
+
+
+def vgg_layers(layer_names):
+    vgg = tf.keras.applications.VGG19(include_top=False, weights="imagenet")
+    vgg.trainable = False
+
+    outputs = [vgg.get_layer(name).output for name in layer_names]
+
+    model = tf.keras.Model([vgg.input], outputs)
+    return model
+
+
+def gram_matrix(input_tensor):
+    result = tf.linalg.einsum("bijc,bijd->bcd", input_tensor, input_tensor)
+    input_shape = tf.shape(input_tensor)
+    num_locations = tf.cast(input_shape[1] * input_shape[2], tf.float32)
+    return result / (num_locations)
+
+
+class StyleContentModel(tf.keras.models.Model):
+    def __init__(self, style_layers, content_layers):
+        super(StyleContentModel, self).__init__()
+        self.vgg = vgg_layers(style_layers + content_layers)
+        self.style_layers = style_layers
+        self.content_layers = content_layers
+        self.num_style_layers = len(style_layers)
+        self.vgg.trainable = False
+
+    def call(self, inputs):
+        "Expects float input in [0,1]"
+        inputs = inputs * 255.0
+        preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
+        outputs = self.vgg(preprocessed_input)
+        style_outputs, content_outputs = (
+            outputs[: self.num_style_layers],
+            outputs[self.num_style_layers :],
+        )
+
+        style_outputs = [gram_matrix(style_output) for style_output in style_outputs]
+
+        content_dict = {
+            content_name: value
+            for content_name, value in zip(self.content_layers, content_outputs)
+        }
+
+        style_dict = {
+            style_name: value
+            for style_name, value in zip(self.style_layers, style_outputs)
+        }
+
+        return {"content": content_dict, "style": style_dict}
+
+
+def clip_0_1(image):
+    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+
+def high_pass_x_y(image):
+    x_var = image[:, :, 1:, :] - image[:, :, :-1, :]
+    y_var = image[:, 1:, :, :] - image[:, :-1, :, :]
+
+    return x_var, y_var
+
+
+def total_variation_loss(image):
+    x_deltas, y_deltas = high_pass_x_y(image)
+    return tf.reduce_sum(tf.abs(x_deltas)) + tf.reduce_sum(tf.abs(y_deltas))
+
+
+def get_flow(image1, image2):
+    flow = False
+    print(image1.shape)
+    return cv2.optflow.createOptFlow_DeepFlow().calc(
+        cv2.cvtColor(image1, cv2.COLOR_RGB2GRAY),
+        cv2.cvtColor(image2, cv2.COLOR_RGB2GRAY),
+        flow,
+    )
+    """
+    return cv2.calcOpticalFlowFarneback(
+        cv2.cvtColor(image1, cv2.COLOR_RGB2GRAY),
+        cv2.cvtColor(image2, cv2.COLOR_RGB2GRAY),
+        None,
+        0.5,
+        3,
+        15,
+        3,
+        5,
+        1.2,
+        0,
+        )
+  """
+
+
+def load_video(path_to_video):
+    vidcap = cv2.VideoCapture(path_to_video)
+    sec = 0
+    count = 0
+    success = getFrame(sec, vidcap)
+    images = []
+    yuvs = []
+    while success:
+        count += 1
+        sec += frame_interval
+        sec = round(sec, 2)
+        success, image = getFrame(sec, vidcap)
+        if success:
+            image_path = str(count) + ".jpg"
+            cv2.imwrite(image_path, image)
+
+            i, j = load_img(image_path)
+            images.append(i)
+            yuvs.append(j)
+            check_output(f"rm -rf {image_path}".split())
+
+    return images, yuvs
+
+
+def convert_to_video(images):
+    frame_array = []
+    path_out = "./stylized_videos/video.avi"
+    size = 0
+
+    for i in images:
+        file_name = "temp.jpg"
+        tensor_to_image(i).save(file_name)
+        img = cv2.imread(file_name)
+        check_output(f"rm -rf {file_name}".split())
+        height, width, layers = img.shape
+        size = (width, height)
+
+        frame_array.append(img)
+    out = cv2.VideoWriter(
+        path_out, cv2.VideoWriter_fourcc(*"DIVX"), 1 / frame_interval, size
+    )
+
+    for i in range(len(frame_array)):
+        # writing to a image array
+        out.write(frame_array[i])
+    out.release()
+
+
+def getFrame(sec, vidcap):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
+    hasFrames, image = vidcap.read()
+
+    return hasFrames, image
+
+
+def check_disocclusion(avg_flow, back_flow):
+    sum = avg_flow + back_flow
+    return (
+        np.sqrt(np.linalg.norm(sum)) ** 2
+        > 0.01 * (np.linalg.norm(avg_flow) ** 2 + np.linalg.norm(back_flow) ** 2) + 0.5
+    )
+
+
+def detect_motion_boundary(u, v):
+    return (u ** 2 + v ** 2) > (0.01 * np.sqrt(u ** 2 + v ** 2) ** 2) + 0.002
 
 
 def loss_function(image, idx, c, omega):
@@ -55,7 +248,7 @@ def loss_function(image, idx, c, omega):
 
     temporal_loss = 0
     if idx > 0:
-        temporal_loss = tf.add_n([tf.reduce_mean((image - omega) ** 2)])
+        temporal_loss = tf.add_n([tf.reduce_mean(((image - omega) * c) ** 2)])
         temporal_loss *= temporal_weight
 
     loss = style_loss + content_loss + temporal_loss
@@ -85,11 +278,29 @@ def train_step(image, omega, c, idx):
     return image, done
 
 
+def preserve_colors(stylized_images, yuvs):
+    sty_images = []
+
+    for idx in range(len(yuvs)):
+        sty = stylized_images[idx]
+        content_yuv = yuvs[idx]
+
+        sty_yuv = cv2.cvtColor(np.float32(sty[0].numpy()), cv2.COLOR_RGB2YUV)
+
+        sty_yuv[:, :, 1:3] = content_yuv[:, :, 1:3]
+
+        img = cv2.cvtColor(sty_yuv, cv2.COLOR_YUV2RGB)
+        img = tf.convert_to_tensor([img], dtype=tf.float32)
+
+        sty_images.append(img)
+    return sty_images
+
+
 if __name__ == "__main__":
     style_path = "images/style.jpg"
-    style_image = load_img(style_path)
+    style_image, _ = load_img(style_path)
 
-    images, warped = load_video("videos/cat3.mp4")
+    images, yuvs = load_video("videos/cat3.mp4")
 
     styled_images = []
     losses = []
@@ -121,7 +332,7 @@ if __name__ == "__main__":
 
         style_weight = 1e-2
         content_weight = 1e4
-        temporal_weight = 4e4  # random veri bare, vi må teste ut ulike verdier for den
+        temporal_weight = 5e4  # random veri bare, vi må teste ut ulike verdier for den
 
         total_variation_weight = 30
 
@@ -132,7 +343,7 @@ if __name__ == "__main__":
             )
         """
 
-        c = tf.zeros(image.shape)
+        c = np.ones(images[idx].shape)
         omega = 0
         if len(styled_images) > 0:
             flow = tf.convert_to_tensor(
@@ -163,27 +374,26 @@ if __name__ == "__main__":
                             int(round(x + backward_flow[0][x][y][0]))
                         ][int(round(y + backward_flow[0][x][y][1]))]
 
-            c = tf.where(
-                (
-                    abs(average_flow + backward_flow) ** 2
-                    > 0.01 * (abs(average_flow) ** 2 + abs(backward_flow) ** 2) + 0.5
-                ),
-                tf.zeros(average_flow.shape),
-                tf.ones(average_flow.shape),
-            )
-
-            omega = tfa.image.dense_image_warp(styled_images[-1], average_flow)
+            for x in range(len(average_flow[0])):
+                for y in range(len(average_flow[0][x])):
+                    if check_disocclusion(
+                        average_flow[0][x][y], backward_flow[0][x][y]
+                    ):  # or detect_motion_boundary(backward_flow[0][x][y][0], backward_flow[0][x][y][1]):
+                        c[0][x][y] = np.zeros(3)
+            content_image = tfa.image.dense_image_warp(styled_images[-1], flow)
+            omega = tfa.image.dense_image_warp(styled_images[-1], flow)
 
         print("content to warped")
-        display.display(tensor_to_image(images[idx - 1]))
+
         display.display(tensor_to_image(images[idx]))
         display.display(tensor_to_image(omega))
+        display.display(tensor_to_image(c))
 
         image = tf.Variable(content_image)
 
         start = time.time()
 
-        epochs = 4
+        epochs = 10
 
         steps_per_epoch = 100
 
@@ -200,7 +410,7 @@ if __name__ == "__main__":
                     prev = styled_images[-1]
 
                 tempimg, done = train_step(image, omega, c, idx)
-                if done:
+                if done and step > 100:
                     optim_done = True
                     break
                 print(".", end="")
@@ -214,4 +424,6 @@ if __name__ == "__main__":
         styled_images.append(image)
 
     # print("Total time: {:.1f}".format(end - start))
+    if keep_colors:
+        styled_images = preserve_colors(styled_images, yuvs)
     convert_to_video(styled_images)
