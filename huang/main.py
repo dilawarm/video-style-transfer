b19ae93c5bf25bb33d400a130e6e61afe6369c37
haavasma <haavasma@stud.ntnu.no>
2020-11-16 14:58:18 +0100

    put in huang implementation with wasserstein-distance as a style metric rather than mean distance and gram-matrix

--- huang/main.py
+++ huang/main.py
@@ -11,14 +11,14 @@ import cv2
 import tensorflow_addons as tfa
 from tqdm import tqdm
 from subprocess import check_output
-from scipy.stats import wasserstein_distance
 
 mpl.rcParams["figure.figsize"] = (12, 12)
 mpl.rcParams["axes.grid"] = False
 os.environ["TFHUB_MODEL_LOAD_FORMAT"] = "COMPRESSED"
 
-frame_interval = 1.0 / 20.0
-loss_tolerance = 0.001
+fps = 15.0
+frame_interval = 1.0/fps
+loss_tolerance = 0.003
 
 
 J = [1, 2, 4, 8]
@@ -36,7 +36,7 @@ def tensor_to_image(tensor):
 
 
 def load_img(path_to_img):
-    max_dim = 512
+    max_dim = 1024
     img = tf.io.read_file(path_to_img)
     img = tf.image.decode_image(img, channels=3)
     img = tf.image.convert_image_dtype(img, tf.float32)
@@ -90,7 +90,7 @@ class StyleContentModel(tf.keras.models.Model):
         self.num_style_layers = len(style_layers)
         self.vgg.trainable = False
 
-    def call(self, inputs):
+    def call(self, inputs, target = False):
         "Expects float input in [0,1]"
         inputs = inputs * 255.0
         preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
@@ -100,8 +100,23 @@ class StyleContentModel(tf.keras.models.Model):
             outputs[self.num_style_layers :],
         )
 
-        # style_outputs = [gram_matrix(style_output) for style_output in style_outputs]
+        styles_output_layers = []
+        if target:
+          for style_output in style_outputs:
+            mean, cov = calc_2_moments(style_output)
 
+            eigvals,eigvects = tf.linalg.eigh(cov)
+            eigroot_mat = tf.linalg.diag(tf.sqrt(tf.maximum(eigvals,0.)))
+            root_cov = tf.matmul(tf.matmul(eigvects, eigroot_mat)
+                                                  ,eigvects,transpose_b=True)
+
+            tr_cov = tf.reduce_sum(tf.maximum(eigvals,0))
+            
+            styles_output_layers.append((mean,
+                                    tr_cov,
+                                    root_cov))
+          style_outputs = styles_output_layers
+            
         content_dict = {
             content_name: value
             for content_name, value in zip(self.content_layers, content_outputs)
@@ -178,7 +193,7 @@ def load_video(path_to_video):
     return images, yuvs
 
 
-def convert_to_video(images, name="video"):
+def convert_to_video(images, name = "video"):
     frame_array = []
     path_out = "./stylized_videos/" + name + ".mp4"
     size = 0
@@ -218,9 +233,7 @@ def check_disocclusion(avg_flow, back_flow):
 
 
 def detect_motion_boundary(u_1, u_2, v_1, v_2):
-    return (u_2 - u_1) ** 2 + (v_2 - v_1) ** 2 > (
-        0.01 * np.sqrt(u_1 ** 2 + v_1 ** 2) ** 2
-    ) + 0.002
+    return (u_2 - u_1) **2  + (v_2 - v_1) ** 2 > (0.01 * np.sqrt(u_1 ** 2 + v_1 ** 2) ** 2) + 0.002
 
 
 def loss_function(image, idx, c, omega):
@@ -233,13 +246,14 @@ def loss_function(image, idx, c, omega):
     style_outputs = outputs["style"]
     content_outputs = outputs["content"]
 
+
+    style_losses = []
+    for name in style_outputs.keys():
+      img_mean, img_conv = calc_2_moments(style_outputs[name])
+      style_losses.append(calc_l2wass_dist(style_targets[name], img_mean, img_conv))
+    
     style_loss = tf.add_n(
-        [
-            wasserstein_distance(
-                style_outputs[name].reshape(-1), style_targets[name].reshape(-1)
-            )
-            for name in style_outputs.keys()
-        ]
+        style_losses
     )
     style_loss *= style_weight / num_style_layers
 
@@ -254,14 +268,53 @@ def loss_function(image, idx, c, omega):
     temporal_loss = 0
     if idx > 0:
         for j in J:
-            if idx - j < 0:
-                break
-            temporal_loss += tf.reduce_mean(((image - omega[j]) * c[j]) ** 2)
+          if idx - j < 0:
+            break
+          temporal_loss += tf.add_n([tf.reduce_mean(((image - omega[j]) * c[j]) ** 2)])
         temporal_loss *= temporal_weight
 
     loss = style_loss + content_loss + temporal_loss
     return loss
 
+
+def calc_2_moments(tensor):
+  """flattens tensor and calculates sample mean and covariance matrix 
+  along last dim (presumably channels)"""
+  
+  shape = tf.shape(tensor, out_type=tf.int32)
+  n = tf.reduce_prod(shape[:-1])
+  
+  flat_array = tf.reshape(tensor, (n, shape[-1]))
+  mu = tf.reduce_mean(flat_array, axis=0, keepdims=True)
+  cov = (tf.matmul(flat_array - mu,flat_array - mu, transpose_a=True)/
+                    tf.cast(n, tf.float32))
+  
+  return mu, cov
+
+
+
+def calc_l2wass_dist(layer_style_desc, mean_synth, cov_synth):
+  """Calculates (squared) l2-Wasserstein distance between gaussians
+  parameterized by first two moments of style and synth activations"""
+  
+  mean_stl, tr_cov_stl, root_cov_stl = layer_style_desc
+  
+  tr_cov_synth = tf.reduce_sum(tf.maximum(
+                tf.linalg.eigh(cov_synth)[0],0.))
+  
+  
+  mean_diff_squared = tf.reduce_sum(tf.square(mean_stl-mean_synth))
+
+  cov_prod = tf.matmul(tf.matmul(root_cov_stl,cov_synth),root_cov_stl)
+  
+  var_overlap = tf.reduce_sum(tf.sqrt(tf.maximum(
+                tf.linalg.eigh(cov_prod)[0],0.1)))
+
+
+  dist = mean_diff_squared+tr_cov_stl+tr_cov_synth-2*var_overlap
+  
+  
+  return dist
 
 def train_step(image, omega, c, idx):
     """
@@ -278,25 +331,24 @@ def train_step(image, omega, c, idx):
     opt.apply_gradients([(grad, image)])
     image.assign(clip_0_1(image))
 
+
     if len(losses) > 50:
-        done = True
-        for i in range(-51, -2):
-            if (losses[i] - losses[i + 1]) > loss_tolerance * losses[i]:
-                done = False
+      done = True
+      for i in range(-51, -2):
+        if (losses[i] - losses[i+1]) > loss_tolerance * losses[i]:
+          done = False
 
     losses.append(loss)
 
     return image, done
 
-
 def warp_flow(img, flow):
     h, w = flow.shape[:2]
     flow = -flow
-    flow[:, :, 0] += np.arange(w)
-    flow[:, :, 1] += np.arange(h)[:, np.newaxis]
+    flow[:,:,0] += np.arange(w)
+    flow[:,:,1] += np.arange(h)[:,np.newaxis]
     res = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
     return res
-
 
 def preserve_colors(stylized_images, yuvs):
     sty_images = []
@@ -315,74 +367,68 @@ def preserve_colors(stylized_images, yuvs):
         sty_images.append(img)
     return sty_images
 
-
 def create_average_flow(flow, backward_flow):
-    average_flow = np.zeros(flow.shape)
-    for x in range(len(flow[0])):
-        for y in range(len(flow[0][x])):
-            x_index = int(round(x + backward_flow[0][x][y][0]))
-            y_index = int(round(y + backward_flow[0][x][y][1]))
-            if x_index < len(flow[0]) and y_index < len(flow[0][x]):
-                average_flow[0][x][y] = flow[0][
-                    int(round(x + backward_flow[0][x][y][0]))
-                ][int(round(y + backward_flow[0][x][y][1]))]
-    return average_flow
+  average_flow = np.zeros(flow.shape)
+  for x in range(len(flow[0])):
+    for y in range(len(flow[0][x])):
+      x_index = int(round(x + backward_flow[0][x][y][0]))
+      y_index = int(round(y + backward_flow[0][x][y][1]))
+      if x_index < len(flow[0]) and y_index < len(flow[0][x]):
+        average_flow[0][x][y] = flow[0][
+                                        int(round(x + backward_flow[0][x][y][0]))
+                                        ][
+                                          int(round(y + backward_flow[0][x][y][1]))
+                                          ]
+  return average_flow
+
 
 
 def create_c_matrix(idx, j, average_flow, backward_flow):
-    c = np.ones(images[idx - j].shape)
-    for x in range(len(average_flow[0])):
-        for y in range(len(average_flow[0][x])):
-            if check_disocclusion(average_flow[0][x][y], backward_flow[0][x][y]) or (
-                x < len(backward_flow[0]) - 1
-                and y < len(backward_flow[0][x]) - 1
-                and detect_motion_boundary(
-                    backward_flow[0][x][y][0],
-                    backward_flow[0][x + 1][y][0],
-                    backward_flow[0][x][y][1],
-                    backward_flow[0][x][y + 1][1],
-                )
-            ):
-                c[0][x][y] = np.zeros(3)
-    return c
+  c = np.ones(images[idx - j].shape)
+  for x in range(len(average_flow[0])):
+    for y in range(len(average_flow[0][x])):
+      if check_disocclusion(average_flow[0][x][y], backward_flow[0][x][y]) or (x < len(backward_flow[0]) - 1 and y < len(backward_flow[0][x]) -1 and detect_motion_boundary(
+                                    backward_flow[0][x][y][0], 
+                                    backward_flow[0][x + 1][y][0],  
+                                    backward_flow[0][x][y][1], 
+                                    backward_flow[0][x][y+1][1])):
+        c[0][x][y] = np.zeros(3)
+  return c
+  
 
 
 def create_flow_lists(idx):
-    flow_list = {}
-    backward_flow_list = {}
-    average_flow_list = {}
+  flow_list = {}
+  backward_flow_list = {}
+  average_flow_list = {}
 
-    for j in J:
-        if idx - j < 0:
-            break
-        flow_list[j] = tf.convert_to_tensor(
-            [get_flow(images[idx - j][0].numpy() * 255, images[idx][0].numpy() * 255)]
-        ).numpy()
-        backward_flow_list[j] = tf.convert_to_tensor(
-            [get_flow(images[idx][0].numpy() * 255, images[idx - j][0].numpy() * 255)]
-        ).numpy()
-        average_flow_list[j] = create_average_flow(flow_list[j], backward_flow_list[j])
-
-    return flow_list, backward_flow_list, average_flow_list
-
+  for j in J:
+    if idx - j < 0:
+      break
+    flow_list[j] = tf.convert_to_tensor([get_flow(images[idx - j][0].numpy() * 255, images[idx][0].numpy() * 255)]).numpy()
+    backward_flow_list[j] = tf.convert_to_tensor([get_flow(images[idx][0].numpy() * 255, images[idx - j][0].numpy() * 255)]).numpy()
+    average_flow_list[j] = create_average_flow(flow_list[j], backward_flow_list[j])
+  
+  return flow_list, backward_flow_list, average_flow_list
 
 def create_c_list(idx, average_flow_list, backward_flow_list):
-    c_list = {}
-    for j in J:
-        if idx - j < 0:
-            break
-        c_list[j] = create_c_matrix(idx, j, average_flow_list[j], backward_flow_list[j])
+  c_list = {}
+  for j in J:
+    if idx - j < 0:
+      break
+    c_list[j] = create_c_matrix(idx, j, average_flow_list[j], backward_flow_list[j])
 
-    return c_list
-
+  return c_list
 
 def create_c_long(idx, j, c_list):
-    sum = np.zeros(c_list[j].shape)
-    for k in J:
-        if k == j:
-            break
-        sum += c_list[k]
-    return np.maximum(c_list[j] - sum, np.zeros(c_list[j].shape))
+  sum = np.zeros(c_list[j].shape)
+  for k in J:
+    if k == j:
+      break
+    sum += c_list[k]
+  return np.maximum(c_list[j] - sum, np.zeros(c_list[j].shape))
+
+
 
 
 if __name__ == "__main__":
@@ -409,18 +455,21 @@ if __name__ == "__main__":
             "block4_conv1",
             "block5_conv1",
         ]
+              
 
         num_content_layers = len(content_layers)
         num_style_layers = len(style_layers)
 
         extractor = StyleContentModel(style_layers, content_layers)
 
-        style_targets = extractor(style_image)["style"]
+        style_targets = extractor(style_image, target = True)["style"]
         content_targets = extractor(content_image)["content"]
 
         opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
 
-        style_weight = 1e-2
+
+        ## 5 stilvekt er bra for 1280 max dim, 2 for 512 max
+        style_weight = 5
         content_weight = 1e4
         temporal_weight = 15e4  # random veri bare, vi må teste ut ulike verdier for den
 
@@ -440,27 +489,25 @@ if __name__ == "__main__":
             flow_dict, backward_flow_dict, average_flow_dict = create_flow_lists(idx)
 
             c_list = create_c_list(idx, average_flow_dict, backward_flow_dict)
-
+            
             omega = {}
             c = {}
             for j in J:
-                if idx - j < 0:
-                    break
-                omega[j] = tf.convert_to_tensor(
-                    [warp_flow(styled_images[idx - j][0].numpy(), flow_dict[j][0])]
-                )
-                c[j] = create_c_long(idx, j, c_list)
-            content_image = tf.convert_to_tensor(
-                [warp_flow(styled_images[-1][0].numpy(), flow_dict[1][0])]
-            )
+              if idx - j < 0:
+                break
+              omega[j] = tf.convert_to_tensor([warp_flow(styled_images[idx - j][0].numpy(), flow_dict[j][0])])
+              c[j] = create_c_long(idx, j, c_list)
+            content_image = tf.convert_to_tensor([warp_flow(styled_images[-1][0].numpy(), flow_dict[1][0])])
+            
 
-        display.display(tensor_to_image(images[idx - 1]))
+
+        display.display(tensor_to_image(images[idx-1]))
         display.display(tensor_to_image(images[idx]))
 
         for j in J:
-            if idx - j < 0:
-                break
-            display.display(tensor_to_image(c[j]))
+          if idx - j < 0:
+            break
+          display.display(tensor_to_image(c[j]))
 
         image = tf.Variable(content_image)
 
@@ -497,6 +544,6 @@ if __name__ == "__main__":
         styled_images.append(image)
 
     # print("Total time: {:.1f}".format(end - start))
-    convert_to_video(styled_images, name="video")
+    convert_to_video(styled_images, name = "video")
     styled_images = preserve_colors(styled_images, yuvs)
-    convert_to_video(styled_images, name="video_preserved_colors")
+    convert_to_video(styled_images, name = "video_preserved_colors")
